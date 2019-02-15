eb03cfd1a8a53b10072047b1baca17540fc91d47
ACVivant <annecharlotte.vivant@gmail.com>
2019-02-15 15:41:57 +0100

    first try to invoque nearbyapi from lunchactivity

--- app/src/main/java/com/vivant/annecharlotte/go4lunch/Models/Restaurant.java
+++ app/src/main/java/com/vivant/annecharlotte/go4lunch/Models/Restaurant.java
@@ -1,5 +1,6 @@
 package com.vivant.annecharlotte.go4lunch.Models;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import androidx.annotation.Nullable;
@@ -12,27 +13,28 @@ public class Restaurant {
     private String restoId;
     private String restoName;
     private List<String> usersToday;
-    private List<String> usersLike;
+    private int distance;
+
 
     public Restaurant() { }
 
-    public Restaurant(String restoId, String restoName, List<String> usersToday, List<String> usersLike) {
+    public Restaurant(String restoId, String restoName, int distance) {
         this.restoId = restoId;
         this.restoName = restoName;
-        this.usersToday = usersToday;
-        this.usersLike = usersLike;
+        this.usersToday = new ArrayList<>();
+        this.distance = distance;
     }
 
     // --- GETTERS ---
     public String getRestoId() { return restoId; }
     public String getRestoName() { return restoName; }
     public List<String> getUsersToday() { return  usersToday;}
-    public List<String> getUsersLike() { return usersLike; }
+    public int getDistance() {return distance;}
 
     // --- SETTERS ---
     public void setRestoId(String restoId) { this.restoId = restoId; }
     public void setRestoName(String restoName) { this.restoName = restoName; }
     public void setUsersToday(List<String> usersToday) {this.usersToday = usersToday;}
-    public void setUsersLike(List<String> usersLike) {this.usersLike = usersLike;}
+    public void setDistance(int distance) {this.distance = distance;}
 }
 
