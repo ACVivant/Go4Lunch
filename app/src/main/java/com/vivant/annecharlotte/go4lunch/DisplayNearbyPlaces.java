f74ea2e2cb718d47ab9bb9173b80b8f46f6c5ef9
ACVivant <annecharlotte.vivant@gmail.com>
2019-02-21 16:13:14 +0100

    link via interface

--- app/src/main/java/com/vivant/annecharlotte/go4lunch/DisplayNearbyPlaces.java
+++ app/src/main/java/com/vivant/annecharlotte/go4lunch/DisplayNearbyPlaces.java
@@ -3,11 +3,12 @@ package com.vivant.annecharlotte.go4lunch;
 import com.vivant.annecharlotte.go4lunch.Models.Nearby.GooglePlacesResult;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Anne-Charlotte Vivant on 21/02/2019.
  */
 public interface DisplayNearbyPlaces {
 
-    public void updateNearbyPlaces(ArrayList<GooglePlacesResult> googlePlacesResults);
+    public void updateNearbyPlaces(List<GooglePlacesResult> googlePlacesResults);
 }
